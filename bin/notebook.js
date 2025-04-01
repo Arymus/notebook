5f43b8300dc6c5b36be38e2e441d90da771457e2
Arymus Reyes <arymusr@gmail.com>
2025-04-01 08:41:06 -0400

    Initial commit

--- bin/notebook.js
+++ bin/notebook.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
-// ^ Allows us to run this file without the node command by referencing the interpreter
+/* ^ Allows us to run this file without the node command
+ * by passing the node interpreter as the first argument in the terminal
+ * so that the second argument (the one we input) is the file name
+ * (in this case an alias for the file, which would be notebook)
+ * 
+ * https://en.wikipedia.org/wiki/Shebang_(Unix)
+ */
 
 const arg = require("arg"); // Import the arg package for parsing arguments
 
 // Try to run a block of code
 try {
+
     // Define the arguments that can be used when we run notebook
     const args = arg({
 	"--help": Boolean, // Add a help flag
@@ -17,8 +24,8 @@ try {
     // If the argument is --help
     if (args["--help"]) {
 
-        // Log all the commands and flags
-        console.log(`Usage:
+        // Log all the commands and flags (it logs all the whitespace when the command is run so this part is ugly)
+        console.log(`Usage: \`notebook [command]\`
 
 notebook start                          Log in to github
 notebook end                            Log out of github
@@ -32,6 +39,7 @@ notebook branch                         Create a new branch
 notebook open                           Switch to a different branch  
 notebook current                        Shows the branch you're in
 notebook view                           View all branches and remote repositories
+notebook copy				Clone an existing repository
 
 Flags:
 
@@ -43,18 +51,18 @@ Flags:
     } else if (args["--version"]) {
         const { version } = require("../package.json"); // Import the version from package.json
         console.log(`notebook v${version}`); // Log the version
-    } else if (args["--dry"]) {
 
-    };
+    // If else (no flags)
+    } else {
 
-    const run = require("../lib/commands"); // Import the run function from commands.js
-
-    // If there are no flags, check for the command
-    if (!process.argv[2].includes("--help") && !process.argv[2].includes("--version")) {
+        // Import the run function from commands.js
+        const run = require("../lib/commands"); 
+    
+        // Call the run function from commands.js with process.argv[2] (or the option) as the command
         run(process.argv[2]);
     };
 
 // Catch any errors
 } catch (e) {
-    console.log(e.message); // Log the error
+    console.log("Error: " + e.message); // Log the error
 };
